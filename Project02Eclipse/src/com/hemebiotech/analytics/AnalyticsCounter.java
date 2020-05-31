eb18b3939abe577f0d4569468f549d6748b5bfbd
claudiuvilau <vilau_claudiu@yahoo.com>
2020-05-31 22:51:44 +0200

    read file with interface

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,6 +8,7 @@ public class AnalyticsCounter {
 	public static void main(String args[]) {
 
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+
 		String filepath_in = "Project02Eclipse/symptoms.txt"; // the path of the file .txt of symptoms and the name
 		List<String> myTxt_symptoms = new ArrayList<String>(); // create the list of the symptoms from the .txt
 		ReadSymptomDataFromFile readDataFromFile = new ReadSymptomDataFromFile(filepath_in);
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -3,18 +3,16 @@ package com.hemebiotech.analytics;
 import java.util.List;
 
 /**
-<<<<<<< HEAD
- * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
-=======
+ * 
+ * <<<<<<< HEAD Anything that will read symptom data from a source The important
+ * part is, the return value from the operation, which is a list of strings,
+ * that may contain many duplications =======
  * 
  * 
  * 
  * Anything that will read symptom data from a source The important part is, the
  * return value from the operation, which is a list of strings, that may contain
- * many duplications
->>>>>>> stash
+ * many duplications >>>>>>> stash
  * 
  * The implementation does not need to order the list
  * 
@@ -23,7 +21,8 @@ public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
 	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	List<String> GetSymptoms();
 }
