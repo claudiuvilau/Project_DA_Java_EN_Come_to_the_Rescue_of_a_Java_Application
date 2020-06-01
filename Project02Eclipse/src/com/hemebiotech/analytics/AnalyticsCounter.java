a55d025333c67babe2ae1b6ba4104bbd1413ae91
claudiuvilau <vilau_claudiu@yahoo.com>
2020-06-01 22:40:38 +0200

    read + count occurrence + sort + write = ok

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,8 @@
 package com.hemebiotech.analytics;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
@@ -10,7 +10,7 @@ public class AnalyticsCounter {
 	public static void main(String args[]) {
 
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-    
+
 		String filepath_in = "Project02Eclipse/symptoms.txt"; // the path of the file .txt of symptoms and the name
 		List<String> myTxt_symptoms = new ArrayList<String>(); // create the list of the symptoms from the .txt
 		ReadSymptomDataFromFile readDataFromFile = new ReadSymptomDataFromFile(filepath_in);
@@ -24,19 +24,16 @@ public class AnalyticsCounter {
 		SortList sortlist_obj = new SortList();
 		list_ordered = sortlist_obj.sort_list(list_ordered);
 
-		System.out.println("My list with the occurrence is : " + list_ordered);
-    
-    		String filepath_out = "result.out";
+		String filepath_out = "result.out";
 
 		WriterTxt wrt_txt = new WriterTxt(filepath_out);
-		Map<String, Integer> list_ordered = new HashMap<String, Integer>();
-
 		wrt_txt.WriteSymptoms(list_ordered);
 
 		success(filepath_out);
 	}
 
-  	private static void success(String filepath_out) {
+	private static void success(String filepath_out) {
 		System.out.println("The file " + filepath_out + " is generated with success ! ");
 
+	}
 }
