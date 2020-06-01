1f50c07d4410ea73bf2986df63cede1ff0849c1a
claudiuvilau <vilau_claudiu@yahoo.com>
2020-06-01 20:46:43 +0200

    read and sort file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,17 +7,16 @@ public class AnalyticsCounter {
 
 	public static void main(String args[]) {
 
-
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
 		String filepath_in = "Project02Eclipse/symptoms.txt"; // the path of the file .txt of symptoms and the name
 		List<String> myTxt_symptoms = new ArrayList<String>(); // create the list of the symptoms from the .txt
 		ReadSymptomDataFromFile readDataFromFile = new ReadSymptomDataFromFile(filepath_in);
 		myTxt_symptoms = readDataFromFile.GetSymptoms();
 
-    List<String> liste = new ArrayList<String>();
+		List<String> liste = new ArrayList<String>();
 		SortList sortlist_obj = new SortList();
 		liste = sortlist_obj.sort_list(liste);
-
+		System.out.println(liste);
 
 	}
 
